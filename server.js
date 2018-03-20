c9c3ed53e9c6c92d930217a9421a3db13206fef8
Michael Reynolds <authormichaelreynolds@gmail.com>
2018-03-20 01:34:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ var port = process.env.PORT || 3000;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(index.html);
+    res.end('Hello World/n');
 }).listen(port);
